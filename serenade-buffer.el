941884e6fd9aca683266e7ffa00ee55f23f3c3d0
Justin Roche <justinroche@Justins-Mini.lan>
2021-06-14 10:58:50 -0400

    add missing filetypes

--- serenade-buffer.el
+++ serenade-buffer.el
@@ -1,13 +1,16 @@
 (defvar serenade-evil nil 
   "If true, use evil commands where possible for default commands")
 
-(defcustom serenade-mode-filetypes '(".js" ".py" ) 
+(defcustom serenade-mode-filetypes 
+  '("js" "py" "c" "h" "cpp" "cc" "cxx" "c++" "hpp" "hh" "hxx" "h++""cs""css" "scss""dart" "go"
+    "html" "vue" "svelte" "java" "js" "jsx" "jsx" "js""jsx" "js" "kt" "py" "rb" "rs" "scss" "sh"
+    "bash" "ts" "tsx" "tsx" "ts""vue" "html")
   "The filetypes that can be used as serenade buffers")
 
 (defun serenade--set-serenade-buffer () 
   (if (and (buffer-file-name) 
            (file-name-extension (buffer-file-name))) 
-      (let* ((ext (format ".%s" (file-name-extension (buffer-file-name))))) 
+      (let* ((ext (format "%s" (file-name-extension (buffer-file-name))))) 
         (if (member ext serenade-mode-filetypes) 
             (setq serenade-buffer (current-buffer) ) 
           (setq serenade-buffer nil ))) 
